164efbb2202a0bdba46f5dce83a964e8b66e42a1
sbx_user1060 <sbx_user1060@ip-10-128-113-161.eu-west-2.compute.internal>
2018-08-18 16:08:09 +0000

    Sat Aug 18 16:08:09 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -4,11 +4,11 @@ package befaster.solutions.FIZ;
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
-        if (number % 15 == 0) {
+        if (number % 15 == 0 || (number.toString().contains("3") && number.toString().contains("5"))) {
             return "fizz buzz";
-        } else if (number % 3 == 0) {
+        } else if (number % 3 == 0 || number.toString().contains("3")) {
             return "fizz";
-        } else if (number % 5 == 0) {
+        } else if (number % 5 == 0 || number.toString().contains("5")) {
             return "buzz";
         } else {
             return number.toString();
--- src/test/java/befaster/solutions/FIZ/FizTest.java
+++ src/test/java/befaster/solutions/FIZ/FizTest.java
@@ -34,4 +34,20 @@ public class FizTest {
     public void number_is_not_multiple_of_five_or_three_should_return_the_number() {
         assertThat("1", equalTo(solution.fizzBuzz(1)));
     }
+
+    @Test
+    public void should_return_fizz_when_number_contains_a_three() {
+        assertThat("fizz", equalTo(solution.fizzBuzz(31)));
+    }
+
+    @Test
+    public void should_return_buzz_when_number_contains_a_five() {
+        assertThat("buzz", equalTo(solution.fizzBuzz(56)));
+    }
+
+    @Test
+    public void should_return_fizz_buzz_when_number_contains_three_and_five() {
+        assertThat("fizz buzz", equalTo(solution.fizzBuzz(352)));
+    }
+
 }
