f3fa786dd359719d18cf86421b9537d9c9ac50f9
sbx_user1060 <sbx_user1060@ip-10-128-113-161.eu-west-2.compute.internal>
2018-08-18 15:37:07 +0000

    Sat Aug 18 15:37:07 UTC 2018

--- src/main/java/befaster/solutions/HLO/HelloSolution.java
+++ src/main/java/befaster/solutions/HLO/HelloSolution.java
@@ -3,6 +3,6 @@ package befaster.solutions.HLO;
 
 public class HelloSolution {
     public String hello(String friendName) {
-        return "Hello World";
+        return "Hello, World!";
     }
 }
